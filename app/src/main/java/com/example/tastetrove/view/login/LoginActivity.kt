7097040abfbc108e2ad343b9ee2918c01e7a51a0
Fillahi Akbar <fillahi099q@Gmail.com>
2024-06-10 20:21:54 +0700

    Initial commit

--- app/src/main/java/com/example/tastetrove/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/tastetrove/view/login/LoginActivity.kt
@@ -80,7 +80,7 @@ class LoginActivity : AppCompatActivity() {
         val email = binding.emailEditText.text.toString()
         viewModel.saveSession(UserModel(email, token))
         AlertDialog.Builder(this).apply {
-            val intent = Intent(context, MainActivity::class.java)
+            val intent = Intent(context, MainActivity()::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
             finish()
